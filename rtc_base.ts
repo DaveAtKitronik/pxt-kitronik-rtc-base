2d5fa83253c339d833adeb021cc9a2728bb1ac7f
DaveAtKitronik <dave.sanderson@kitronik.co.uk>
2020-03-27 14:08:56 +0000

    Update rtc_base.ts
    Only initialise battery pin if we need to.
    Use write number for a single byte, rather than a 2 byte buffer.

--- rtc_base.ts
+++ rtc_base.ts
@@ -112,12 +112,6 @@ namespace kitronik_RTC {
         let readCurrentSeconds = 0
         let readWeekDayReg = 0
 
-        //Seconds register read for current seconds for when masking start RTC bit
-        writeBuf[0] = RTC_SECONDS_REG
-        pins.i2cWriteBuffer(CHIP_ADDRESS, writeBuf, false)
-
-        readBuf = pins.i2cReadBuffer(CHIP_ADDRESS, 1, false)
-        readCurrentSeconds = readBuf[0]
 
         // First set the external oscillator
         writeBuf[0] = RTC_CONTROL_REG
@@ -126,14 +120,23 @@ namespace kitronik_RTC {
 
         //Reading weekday register so can mask the Battery backup supply
         writeBuf[0] = RTC_WEEKDAY_REG
-        pins.i2cWriteBuffer(CHIP_ADDRESS, writeBuf, false)
+        pins.i2cWritenumber(CHIP_ADDRESS, RTC_WEEKDAY_REG,UInt8LE, false)
         readBuf = pins.i2cReadBuffer(CHIP_ADDRESS, 1, false)
         readWeekDayReg = readBuf[0]
+	      if((readWeekDayReg & ENABLE_BATTERY_BACKUP)==0) //we need to set the battery backup bit
+	      {
+        	writeBuf[0] = RTC_WEEKDAY_REG
+        	writeBuf[1] = ENABLE_BATTERY_BACKUP | readWeekDayReg             //logic OR the two bytes together for new value
+          pins.i2cWriteBuffer(CHIP_ADDRESS, writeBuf, false)       //write to enable battery backup and mask with current reading of register
+        }
+	    
+        //Seconds register read for current seconds for when masking start RTC bit
+        writeBuf[0] = RTC_SECONDS_REG
+        pins.i2cWriteNumber(CHIP_ADDRESS,RTC_SECONDS_REG, UInt8LE, false)
 
-        writeBuf[0] = RTC_WEEKDAY_REG
-        writeBuf[1] = ENABLE_BATTERY_BACKUP | readWeekDayReg             //logic OR the two bytes together for new value
-        pins.i2cWriteBuffer(CHIP_ADDRESS, writeBuf, false)       //write to enable battery backup and mask with current reading of register
-
+        readBuf = pins.i2cReadBuffer(CHIP_ADDRESS, 1, false)
+        readCurrentSeconds = readBuf[0]
+	    
         //Block write to start oscillator
         writeBuf[0] = RTC_SECONDS_REG
         writeBuf[1] = START_RTC | readCurrentSeconds
